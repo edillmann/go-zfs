d37134c3ca1fb9c17025b43c6f2ff244cb0a766f
Eric Dillmann <edillmann@albalogic.fr>
2016-02-21 20:29:45 +0100

    fix depth handling

--- utils.go
+++ utils.go
@@ -303,7 +303,7 @@ func parseInodeChanges(lines [][]string) ([]*InodeChange, error) {
 func (z *ZfsH) listByType(t, filter string, depth int) ([]*Dataset, error) {
 	args := []string{"list", "-rH", "-t", t, "-o", strings.Join(DsPropList, ",")}
 
-	if depth > 0 {
+	if depth > -1 {
 		args = append(args, "-d", strconv.Itoa(depth))
 	}
 	if filter != "" {
