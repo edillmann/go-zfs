38f22804cb851b7928cfa8fa3560977211c5156f
Eric Dillmann <edillmann@albalogic.fr>
2017-09-28 11:45:11 +0200

    add zsync:from

--- utils_notsolaris.go
+++ utils_notsolaris.go
@@ -3,7 +3,7 @@
 package zfs
 
 // List of ZFS properties to retrieve from zfs list command on a non-Solaris platform
-var DsPropList = []string{"name", "origin", "used", "available", "mountpoint", "compression", "type", "volsize", "quota", "written", "logicalused", "receive_resume_token"}
+var DsPropList = []string{"name", "origin", "used", "available", "mountpoint", "compression", "type", "volsize", "quota", "written", "logicalused", "receive_resume_token", "compressratio", "usedbysnapshots"}
 
 // List of Zpool properties to retrieve from zpool list command on a non-Solaris platform
 var ZpoolPropList = []string{"name", "health", "allocated", "size", "free"}
